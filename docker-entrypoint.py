c92ae032068643d3158f0af62946e071ffd0d0ac
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-04-11 17:35:59 -0400

    Cleanup legacy code.

--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -70,20 +70,6 @@ def http_construct(args, unknown):
 
     return str_http
 
-def pman_do(args, unknown):
-
-    str_otherArgs   = ' '.join(unknown)
-
-    str_CMD = "/usr/local/bin/pman %s" % (str_otherArgs)
-    return str_CMD
-
-def pfioh_do(args, unknown):
-
-    str_otherArgs   = ' '.join(unknown)
-
-    str_CMD = "/usr/local/bin/pfioh %s" % (str_otherArgs)
-    return str_CMD
-
 def purl_do(args, unknown):
 
     str_http        = http_construct(args, unknown)
@@ -95,36 +81,28 @@ def purl_do(args, unknown):
     str_CMD = "/usr/local/bin/purl --verb %s %s %s --jsonwrapper '%s' --msg '%s' %s" % (args.verb, str_raw, str_http, args.jsonwrapper, args.msg, str_otherArgs)
     return str_CMD
 
-def bash_do(args, unknown):
-
-    str_http        = http_construct(args, unknown)
-    str_otherArgs   = ' '.join(unknown)
-
-    str_CMD = "/bin/bash"
-    return str_CMD
-
 
 parser  = argparse.ArgumentParser(description = str_desc)
 
-parser.add_argument(
-    'app',
-    nargs   = '?',
-    default = 'pman'
-)
-parser.add_argument(
-    '--pman',
-    action  = 'store_true',
-    dest    = 'b_pman',
-    default = False,
-    help    = 'if specified, indicates transmission to a linked <pman> container.',
-)
-parser.add_argument(
-    '--pfioh',
-    action  = 'store_true',
-    dest    = 'b_pfioh',
-    default = False,
-    help    = 'if specified, indicates transmission to a linked <pfioh> container.',
-)
+#parser.add_argument(
+    #'app',
+    #nargs   = '?',
+    #default = 'pman'
+#)
+#parser.add_argument(
+    #'--pman',
+    #action  = 'store_true',
+    #dest    = 'b_pman',
+    #default = False,
+    #help    = 'if specified, indicates transmission to a linked <pman> container.',
+#)
+#parser.add_argument(
+    #'--pfioh',
+    #action  = 'store_true',
+    #dest    = 'b_pfioh',
+    #default = False,
+    #help    = 'if specified, indicates transmission to a linked <pfioh> container.',
+#)
 parser.add_argument(
     '--msg',
     action  = 'store',
@@ -161,7 +139,7 @@ args, unknown   = parser.parse_known_args()
 
 if __name__ == '__main__':
     try:
-        fname   = '%s_do(args, unknown)' % args.app
+        fname   = 'purl_do(args, unknown)'
         str_cmd = eval(fname)
         print(str_cmd)
         os.system(str_cmd)
